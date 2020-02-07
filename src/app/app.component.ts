3583ae431492cb19782f222e0355d7f3d4c58434
adal09 <lucio_sistemas@live.com>
2020-02-07 13:42:36 -0600

    Carga principal de archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LoginApp';
+  //title = 'LoginApp';
+  
+  userIngresado: User = new User();
+
+  isLogin(){
+    console.log(this.userIngresado);
+    this.userIngresado = new User();  
+    
+  }
+
+
+
 }
